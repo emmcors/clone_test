473388aa7c910a15050394cd7ab154bb38893f47
emmcors <emmcors@hotmail.com>
2024-06-25 16:57:19 -0600

    Agragamos un if

--- my_script.py
+++ my_script.py
@@ -1,4 +1,9 @@
 import os
 
 print(f'Mi directorio de trabajo es: {os.getcwd()}')
-print('hola')
+
+x = 10
+if x % 2 == 0:  # verifica si es un número par
+    print(f"{x} es un numero par")
+else:
+    print(f"{x} es un numero impar")
